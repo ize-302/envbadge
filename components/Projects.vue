060be709ec3683db1a4bc51100a23532487171f3
ize-302 <adavizeozorku@gmail.com>
2023-10-25 14:33:34 +0100

    :sparkles: Add empty state

--- components/Projects.vue
+++ components/Projects.vue
@@ -13,11 +13,23 @@
       :key="index"
     />
   </div>
+  <div
+    v-show="!store.getLoadingStatus && store.getProjects.items.length === 0"
+    class="my-10 flex flex-col justify-center items-center w-full gap-5 text-slate-400 dark:text-gray-600"
+  >
+    <ArchiveOffIcon
+      class="text-slate-200 dark:text-gray-800"
+      size="72"
+      stroke-width="1"
+    />
+    You have no projects yet!
+  </div>
 </template>
 
 <script setup>
 import { useStore } from "~/store";
 import Project from "./Project.vue";
+import { ArchiveOffIcon, TrashIcon } from "vue-tabler-icons";
 const store = useStore();
 
 store.fetchProjects().then(() => store.updateLoadingStatus(false));
