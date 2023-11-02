c82309048dc158236d1753792c79f3d77c62062d
ize-302 <adavizeozorku@gmail.com>
2023-11-02 15:17:25 +0100

    :chart_with_upwards_trend: Update vercel-analytics config

--- app.config.ts
+++ app.config.ts
@@ -5,4 +5,12 @@ export default defineAppConfig({
   colorMode: {
     preference: "light",
   },
+  vercelAnalytics: {
+    mode: "auto",
+    debug: true,
+    beforeSend: (event: { url: string | string[] }) => {
+      if (event.url.includes("/private")) return null;
+      return event;
+    },
+  },
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ const AUTH_ORIGIN = process.env.AUTH_ORIGIN as string;
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt", "@sidebase/nuxt-auth"],
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@sidebase/nuxt-auth",
+    "nuxt-vercel-analytics",
+  ],
   auth: {
     origin: AUTH_ORIGIN,
     basePath: "/api/auth/callback/github",
